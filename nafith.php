2a3c402019fe6555cb992ddb1b73bb31e04b74df
foodicsRohan <145523082+foodicsRohan@users.noreply.github.com>
2023-10-13 09:55:45 +0530

    Update nafith.php
    teste

--- nafith.php
+++ nafith.php
@@ -3,9 +3,6 @@
 
 // CONSTANTS:
 
-
-// Nafith BODY  FILE IN THE FOLDER
-const FILE_TO_UPLOAD_NAME = "NafithBoby.txt";
 const Authorization = "MnlkZnJjWVBNOVNyWW9lZU5pcWR5ZDNkVDJkdzdmeTVyWFJnMVhXajpPakRiVXFNSEVlcDdQUUVvVnRpTmJhUWpnYnc2MlUxTG5TU2c4cnBkRW1CZlRHQTFGSW9KNTNQY3dKMDVQWTllZW5aMTNHTExXZ2p4emg4eW9TQlo0aVViSTZSenI0WUI5QnR2amltVUF3eDE5M3lNU3RnS3ZFMHlhUjQyOWxldQ==";
 const secret_key = "rwGoV7s8dBY1J7zZjcJAjWOazEVs0dIsEw7KUHEkiBAwUcTnmQV8pgXkMCrXdIVNQLguTNTwQPW4aj81rM42TIJcY3UC22iOhuTDq1ZxsYa0IggWpu2qjf1LwhexDqrs";
 
@@ -17,7 +14,6 @@ function debugToScreen(string $message): void
 {
     if (CODE_DEBUG_FLAG) {
         print "DEBUG MESSAGE: $message\r\n";
-        //file_put_contents(FILE_TO_UPLOAD_NAME, "DEBUG MESSAGE: $message\r\n" );
     }
 
 }
